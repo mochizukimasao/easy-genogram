09f56f4f44ba4b5299c804f57bd7236787202bde
松浦康介 <mochizuki@mkosukeM1-Mac-mini.local>
2025-07-19 13:03:04 +0900

    Update eraser icon with new SVG from user

--- components/icons/UtilIcons.tsx
+++ components/icons/UtilIcons.tsx
@@ -51,18 +51,13 @@ export const DivorceIcon: React.FC<IconProps> = ({ className }) => (
 
 export const EraseIcon: React.FC<IconProps> = ({ className }) => (
   <svg
-    viewBox="0 0 24 24"
-    className={className}
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
     xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    className={className}
+    viewBox="0 0 16 16"
   >
-    {/* Eraser icon resembling the provided image */}
-    <rect x="4" y="8" width="16" height="8" rx="1" ry="1"/>
-    <line x1="10" y1="8" x2="10" y2="16"/>
-    <line x1="14" y1="8" x2="14" y2="16"/>
+    <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293z"/>
   </svg>
 );
